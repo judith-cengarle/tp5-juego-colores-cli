b989451f59661015f0954487a90df0d41eaf67d1
Judith Cengarle <judithcengarle@gmail.com>
2020-06-05 22:48:16 -0300

    Header component created.

---
+++ src/components/Game.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="game-components">
+    <div id="body">
+      <Header/>
+
+
+
+
+
+
+    </div>
+
+  </section>
+</template>
+
+<script>
+
+  import Header from './Header.vue'
+
+  export default {
+    name: 'game-components',
+    props: [],
+    components : {
+      Header
+    }
+  }
+</script>
+
+
+<style scoped>
+  .game-components {
+    background: #232323;
+  }
+  #body {
+    background: #232323;
+    margin: 0;
+    font-family: "Montserrat", "Avenir";
+  }
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    
+    <section class="game-components" >
+    <div id="header">
+      <h1>The Great <br>
+      
+      <br>
+      Guessing Game</h1>
+    </div>
+  </section>
+
+</template>
+
+<script>
+    export default {
+        name: 'game-components',
+        props: {
+
+        }        
+        
+    }
+</script>
+
+<style scoped>
+    #header {
+    transition: all 0.3s;    
+    text-transform: uppercase;
+    text-align: center;
+    margin: 0;
+    color: white;	
+    }
+</style>
